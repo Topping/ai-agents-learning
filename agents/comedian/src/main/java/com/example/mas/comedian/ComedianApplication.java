03dadd7199e88c882dd7d74752648dfbc8ab1d7a
bingo <itsme>
2025-11-05 00:00:20 +0100

    more

--- agents/comedian/src/main/java/com/example/mas/comedian/ComedianApplication.java
+++ agents/comedian/src/main/java/com/example/mas/comedian/ComedianApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import(AdkWebServer.class)
 public class ComedianApplication {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.setProperty("adk.agents.loader", "none");
         SpringApplication.run(ComedianApplication.class, args);
     }
